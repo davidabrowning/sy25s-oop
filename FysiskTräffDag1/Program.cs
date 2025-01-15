cf554bc0978a78c488a49b3a5c3fdf8353a6ab78
David Browning <david.a.browning@gmail.com>
2025-01-15 16:09:47 +0100

    Övningsprogram på fysisk träff dag 1.

---
+++ FysiskTräffDag1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FysiskTräffDag1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad heter du?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hej " + name);
+        }
+    }
+}
---
+++ HelloWorld2/HelloWorld2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// ================================ NAMESPACE =================================
+// HelloWorld2. Program som skriver ut Hello World.
+// ============================================================================
+namespace HelloWorld2
+{
+
+    // ================================ KLASS =================================
+    // Program. Innehåller Main.
+    // ========================================================================
+    internal class Program
+    {
+
+        // ============================== METOD ===============================
+        // Main. Start på programmet.
+        // ====================================================================
+        static void Main(string[] args)
+        {
+            string hälsning = "Hello world!";
+            Greet(hälsning);
+        }
+
+        // ============================== METOD ===============================
+        // Greet. Skriver ut ett meddelande.
+        // ====================================================================
+        private static void Greet(string meddelande)
+        {
+            Console.WriteLine("Början på hälsning.");
+            Console.WriteLine(meddelande);
+            Console.WriteLine("Slut på hälsning.");
+        }
+    }
+}
