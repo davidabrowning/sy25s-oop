40ad9985260c410e707b9bf9af8b675564d91db8
David Browning <david.a.browning@gmail.com>
2025-01-20 11:51:32 +0100

    Practice enums.

---
+++ Landsdelar/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Landsdelar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Landsdel kalleLandsdel = Landsdel.Norrland;
+            Landsdel lisaLandsdel = Landsdel.Svealand;
+            Landsdel algotLandsdel = Landsdel.Götaland;
+
+            Console.WriteLine("Skriver ut elevernas landsdelar:");
+            Console.WriteLine("Kalle: " + kalleLandsdel);
+            Console.WriteLine("Lisa: " + lisaLandsdel);
+            Console.WriteLine("Algot: " + algotLandsdel);
+        }
+    }
+
+    internal enum Landsdel
+    {
+        Norrland,
+        Svealand,
+        Götaland
+    }
+}
