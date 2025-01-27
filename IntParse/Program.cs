37d3e63b98c9ad72513f820afdaccd19676d38b9
David Browning <david.a.browning@gmail.com>
2025-01-27 15:55:55 +0100

    OOPIntro program.

---
+++ IntParse/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace IntParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange ett heltal:");
+            try
+            {
+                string indata = Console.ReadLine();
+                int i = int.Parse(indata);
+                Console.WriteLine($"You entered {i} (Parse)");
+                int j = Convert.ToInt32(indata);
+                Console.WriteLine($"You entered {j} (Convert)");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ogiltig format.");
+            }
+        }
+    }
+}
