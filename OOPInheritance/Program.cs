c2632648620e9d84e9329d9858f8adc785169801
David Browning <david.a.browning@gmail.com>
2025-01-30 15:03:26 +0100

    Practicing inheritance.

---
+++ OOPInheritance/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace OOPInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("Fredrik", "Terent");
+            Console.WriteLine(p1.Salute());
+            Console.WriteLine(p1);
+
+            Student s1 = new Student("John", "Doe");
+            s1.Course = "Math";
+            Console.WriteLine(s1.Salute());
+            Console.WriteLine(s1);
+
+            Teacher t1 = new Teacher();
+            t1.FirstName = "Jane";
+            t1.LastName = "Roe";
+            Console.WriteLine(t1.Salute());
+            Console.WriteLine(t1);
+
+        }
+    }
+}
