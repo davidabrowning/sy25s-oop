853d7ef499bd27ffc1df37d43a6c0873b41bafc9
David Browning <david.a.browning@gmail.com>
2025-01-30 15:03:43 +0100

    Vehicles classes to practice inheritance.

---
+++ Vehicles/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vehicles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Vehicle> vehicles = new List<Vehicle>();
+            Car c1 = new Car();
+            Car c2 = new Car();
+            Tractor t1 = new Tractor();
+            Tractor t2 = new Tractor();
+            Motorcycle m1 = new Motorcycle();
+            Motorcycle m2 = new Motorcycle();
+            Tricycle y1 = new Tricycle();
+            Tricycle y2 = new Tricycle();
+
+            vehicles.Add(c1);
+            vehicles.Add(c2);
+            vehicles.Add(t1);
+            vehicles.Add(t2);
+            vehicles.Add(m1);
+            vehicles.Add(m2);
+            vehicles.Add(y1);
+            vehicles.Add(y2);
+
+            c1.Run(10);
+            c2.Run(99);
+            t1.Run(10);
+            t2.Run(99);
+            m1.Run(10);
+            m2.Run(99);
+            y1.Run(10);
+            y2.Run(99);
+
+            PrintAllVehicles(vehicles);
+        }
+
+        private static void PrintAllVehicles(List<Vehicle> vehicles)
+        {
+            foreach (Vehicle v in vehicles)
+            {
+                Console.WriteLine(v);
+            }
+        }
+    }
+}
---
+++ Vehicles/Tractor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    public class Tractor : Vehicle
+    {
+        private int maxSpeed = 30;
+        public override int Speed
+        {
+            get { return speed; }
+            set { speed = Math.Min(value, maxSpeed); }
+        }
+        public Tractor() : base()
+        {
+            WheelCount = 4;
+        }
+        public override void Run(int speed)
+        {
+            this.Speed = Math.Min(speed, this.maxSpeed);
+        }
+    }
+}
