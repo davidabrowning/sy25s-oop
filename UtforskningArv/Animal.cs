c15437d58737602ae26975054eb71f70684923f0
David Browning <david.a.browning@gmail.com>
2025-02-01 16:40:54 +0100

    Practice activities from Fredrik.

---
+++ UtforskningArv/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtforskningArv
+{
+    public abstract class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; private set; }
+        protected Animal(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public abstract void MakeNoise();
+        public override string ToString()
+        {
+            return $"{Name}, {Age} år gammal";
+        }
+    }
+}
---
+++ UtforskningKlasser/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace UtforskningKlasser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car(model: "Cherokee", make: "Jeep", year: 1995, color: "Blue");
+            myCar.ShowInfo();
+        }
+    }
+}
