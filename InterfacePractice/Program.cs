89deb7944b4ed2645e82637b8cc21614aed5d73d
David Browning <david.a.browning@gmail.com>
2025-02-01 01:46:11 +0100

    Practice using interfaces.

---
+++ InterfacePractice/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace InterfacePractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IDriveable> vehicles = new List<IDriveable>();
+            vehicles.Add(new Car());
+            vehicles.Add(new Motorcycle());
+            foreach (IDriveable vehicle in vehicles)
+            {
+                Console.WriteLine("============");
+                Console.WriteLine(vehicle);
+                vehicle.FillTank();
+                Console.WriteLine(vehicle);
+                vehicle.Drive();
+                Console.WriteLine(vehicle);
+                vehicle.Drive();
+                Console.WriteLine(vehicle);
+                vehicle.Drive();
+                Console.WriteLine(vehicle);
+                vehicle.FillTank();
+                Console.WriteLine(vehicle);
+            }
+            Console.WriteLine("============");
+        }
+    }
+}
