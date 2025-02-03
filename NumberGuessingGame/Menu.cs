f54477f95ee676b53335c47244e073ead3a7b147
David Browning <david.a.browning@gmail.com>
2025-02-03 09:59:17 +0100

    Initial development on NumberGuessingGame.

---
+++ NumberGuessingGame/Menu.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessingGame
+{
+    public class Menu
+    {
+        private bool run = true;
+        private UserInputRetriever userInputRetriever = new UserInputRetriever();
+        public void Display()
+        {
+            while(run)
+            {
+                PrintMenu();
+                ProcessMenuSelection();
+            }
+        }
+        private void PrintMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("===== Main menu =====");
+            Console.WriteLine("[P]lay game");
+            Console.WriteLine("[L]ist high scores");
+            Console.WriteLine("[Q]uit");
+        }
+        private void ProcessMenuSelection()
+        {
+            switch (userInputRetriever.GetStringInput("Your choice: ", 1, 1).ToUpper())
+            {
+                case "P":
+                    StartGame();
+                    break;
+                case "L":
+                    PrintHighScores();
+                    break;
+                case "Q":
+                    run = false;
+                    break;
+                default:
+                    ProcessMenuSelection(); // Try again
+                    break;
+            }
+        }
+        private void StartGame()
+        {
+            Console.WriteLine("Starting game...");
+        }
+        private void PrintHighScores()
+        {
+            Console.WriteLine("Printing high scores...");
+        }
+    }
+}
---
+++ NumberGuessingGame/UserInputRetriever.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessingGame
+{
+    public class UserInputRetriever
+    {
+        public string GetStringInput(string prompt, int min, int max)
+        {
+            string input = "";
+            do
+            {
+                Console.Write(prompt);
+                input = Console.ReadLine().Trim();
+            } while (input.Length < min || input.Length > max);
+            return input;
+        }
+        public int GetIntInput(string prompt, int min, int max)
+        {
+            int input = 0;
+            do
+            {
+                Console.Write(prompt);
+                try
+                {
+                    input = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    continue; // Try again
+                }
+            } while (input < min || input > max);
+            return input;
+        }
+    }
+}
