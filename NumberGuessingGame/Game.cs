5930a5f3d95d55fd739a7048f4c0edfb5354ce91
David Browning <david.a.browning@gmail.com>
2025-02-03 10:55:37 +0100

    Add LatestGuess property.

--- NumberGuessingGame/Game.cs
+++ NumberGuessingGame/Game.cs
@@ -9,16 +9,34 @@ namespace NumberGuessingGame
     public class Game
     {
         // Private constants
-        private int targetNumberMin = 1;
-        private int targetNumberMax = 100;
+        private static int targetNumberMin = 1;
+        private static int targetNumberMax = 100;
 
         // Private instance variables
         private UserInputRetriever userInputRetriever = new UserInputRetriever();
 
         // Public properties
-        public int Guesses { get; } = 0;
+        public int LatestGuess { get; private set; } = targetNumberMin - 1;
+        public int Guesses { get; private set; } = 0;
+        public int TargetNumber { get; private set; }
+
+        // Constructors
+        public Game()
+        {
+            TargetNumber = GenerateTargetNumber();
+        }
 
         // Methods
+        private int GenerateTargetNumber()
+        {
+            Random rand = new Random();
+            return rand.Next(targetNumberMin, targetNumberMax + 1);
+        }
+        private void Guess(int guess)
+        {
+            LatestGuess = guess;
+            Guesses++;
+        }
         public static void RunTests()
         {
             // Variables that can be reused
@@ -30,6 +48,16 @@ namespace NumberGuessingGame
             title = "Guesses starts at 0";
             game = new Game();
             TestHelper.AssertEquals(title, 0, game.Guesses);
+
+            title = "Guesses is 1 after 1 guess";
+            game = new Game();
+            game.Guess(5);
+            TestHelper.AssertEquals(title, 1, game.Guesses);
+
+            title = "LatestGuess reflects most recent guess";
+            game = new Game();
+            game.Guess(7);
+            TestHelper.AssertEquals(title, 7, game.LatestGuess);
         }
     }
 }
