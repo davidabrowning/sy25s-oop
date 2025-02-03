7a2ece5ddb7e8c8387ab954b64347c4e271cf748
David Browning <david.a.browning@gmail.com>
2025-02-03 10:26:34 +0100

    Add Game class.

---
+++ NumberGuessingGame/Game.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessingGame
+{
+    public class Game
+    {
+        // Private constants
+        private int targetNumberMin = 1;
+        private int targetNumberMax = 100;
+
+        // Private instance variables
+        private UserInputRetriever userInputRetriever = new UserInputRetriever();
+
+        // Public properties
+        public int Guesses { get; } = 0;
+
+        // Methods
+        public static void RunTests()
+        {
+            // Variables that can be reused
+            string title;
+            Game game;
+
+            Console.WriteLine("Running Game tests:");
+
+            title = "Guesses starts at 0";
+            game = new Game();
+            TestHelper.AssertEquals(title, 0, game.Guesses);
+        }
+    }
+}
---
+++ NumberGuessingGame/TestHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessingGame
+{
+    public static class TestHelper
+    {
+        public static void PrintSuccess(string title)
+        {
+            Console.WriteLine($"Success: {title}");
+        }
+        public static void PrintFailure(string title, string expected, string actual)
+        {
+            Console.WriteLine($"Failure: {title} | Expected: {expected} | Actual: {actual}");
+        }
+        public static void AssertTrue(string title, bool actual)
+        {
+            if(actual)
+            {
+                PrintSuccess(title);
+            }
+            else
+            {
+                PrintFailure(title, false.ToString(), actual.ToString());
+            }
+        }
+        public static void AssertFalse(string title, bool actual)
+        {
+            if (!actual)
+            {
+                PrintSuccess(title);
+            }
+            else
+            {
+                PrintFailure(title, false.ToString(), actual.ToString());
+            }
+        }
+        public static void AssertEquals(string title, Object expected, Object actual)
+        {
+            if (expected.Equals(actual))
+            {
+                PrintSuccess(title);
+            }
+            else
+            {
+                PrintFailure(title, expected.ToString(), actual.ToString());
+            }
+        }
+    }
+}
