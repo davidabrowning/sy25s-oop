5c85232ac42592337f8630e8343d7281a84edc4f
David Browning <david.a.browning@gmail.com>
2025-02-12 00:17:57 +0100

    Fix file load issue.

--- ParProgrammeringStrumpor/SockApp.cs
+++ ParProgrammeringStrumpor/SockApp.cs
@@ -18,9 +18,9 @@ namespace ParProgrammeringStrumpor
         private readonly string[] MenuOptions =
         {
             $"{(int)MainMenu.AddSock}. Lägg till en socka",
-            $"{(int)MainMenu.SaveSocks}. Spara sockar",
-            $"{(int)MainMenu.LoadFromFile}. Ladda sockar",
-            $"{(int)MainMenu.DisplayAll}. Skriv ut sockar",
+            $"{(int)MainMenu.SaveSocks}. Spara sockor",
+            $"{(int)MainMenu.LoadFromFile}. Ladda sockor",
+            $"{(int)MainMenu.DisplayAll}. Skriv ut sockor",
             $"{(int)MainMenu.Quit}. Avsluta programmet"
         };
         private const string AddTitle = "Lägg till socka";
@@ -33,18 +33,24 @@ namespace ParProgrammeringStrumpor
         private const string ColorPrompt = "Ange färg";
         private const string GradePrompt = "Ange betyg (1-5 där 1 är bäst)";
         private const string AddSockSucceeded = "Lyckades lägga till ny socka.";
-        private const string UpdateSaveStarted = "Sparar Davids sockar til fil...";
-        private const string UpdateSaveSucceeded = "Socklistan sparad.";
-        private const string UpdateSaveFailed = "Lyckades inte spara til fil.";
-        private const string UpdateLoadStarted = "Läser in Sigges sockar från fil...";
-        private const string UpdateLoadSucceeded = "Lyckades läsa in från fil.";
-        private const string UpdateLoadFailed = "Lyckades inte läsa in socklistan från filen.";
         private const string QuitMessage = "Programmet avslutas. Tack och hej då!";
+        private const string TitleDavidsSocks = "Davids sockor (från användaren)";
+        private const string TitleSiggesSocks = "Sigges sockor (från fil)";
 
         // Private fields
-        private Printer printer = new Printer();
-        private List<Sock> davidsSocks = new List<Sock>();
-        private List<Sock> siggesSocks = new List<Sock>();
+        private Printer printer;
+        private DataTransfer dataTransfer;
+        private List<Sock> davidsSocks;
+        private List<Sock> siggesSocks;
+
+        // Constructor
+        public SockApp()
+        {
+            printer = new Printer();
+            dataTransfer = new DataTransfer(printer);
+            davidsSocks = new List<Sock>();
+            siggesSocks = new List<Sock>();
+        }
 
         public void Run()
         {
@@ -143,48 +149,22 @@ namespace ParProgrammeringStrumpor
         private void Save()
         {
             printer.PrintTitle(SaveTitle);
-            printer.PrintUpdate(UpdateSaveStarted);
-
-            try
-            {
-                string sockListJson = JsonSerializer.Serialize(davidsSocks);
-                File.WriteAllText("socklista.json", sockListJson);
-                printer.PrintUpdate(UpdateSaveSucceeded);
-            }
-            catch (Exception ex)
-            {
-                printer.PrintUpdate(UpdateSaveFailed);
-                printer.PrintUpdate(ex.Message);
-            }
-
+            dataTransfer.SaveSockListToFile(davidsSocks);
             ReturnToMenu();
         }
 
         private void Load()
         {
             printer.PrintTitle(LoadTitle);
-            printer.PrintUpdate(UpdateLoadStarted);
-
-            try
-            {
-                string sockListFileText = File.ReadAllText("siggessockar.json");
-                siggesSocks = JsonSerializer.Deserialize<List<Sock>>(sockListFileText) ?? new List<Sock>();
-                printer.PrintUpdate(UpdateLoadSucceeded);
-            }
-            catch (Exception ex)
-            {
-                printer.PrintUpdate(UpdateLoadFailed);
-                printer.PrintUpdate(ex.Message);
-            }
-
+            siggesSocks = dataTransfer.LoadSockListFromFile();
             ReturnToMenu();
         }
 
         private void DisplayAllSocks()
         {
             printer.PrintTitle(DisplayAllTitle);
-            DisplaySockList("Davids sockar", davidsSocks);
-            DisplaySockList("Sigges sockar (från fil)", siggesSocks);
+            DisplaySockList(TitleDavidsSocks, davidsSocks);
+            DisplaySockList(TitleSiggesSocks, siggesSocks);
             ReturnToMenu();
         }
 
