551542dc132401a831d66ba186a3e5681a4a41ae
David Browning <david.a.browning@gmail.com>
2025-02-24 20:36:07 +0100

    Generics homework exercise.

---
+++ HemläxaGenerics/HemläxaGenerics/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HemläxaGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            Student student1 = new Student("John");
+            Student student2 = new Student("Jane");
+
+            FredriksHelper.PrintParameters(32, 34);
+            FredriksHelper.PrintParameters("Hello", "Hej");
+            FredriksHelper.PrintParameters(student1, student2);
+        }
+    }
+}
